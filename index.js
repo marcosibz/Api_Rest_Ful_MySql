9cebfe619950343b52641c9c2bd32937471eb327
marcosibz <ibanez.marcos@tecnica7.edu.ar>
2025-10-01 20:18:18 -0300

    probamos los metodos get post put y delete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import pool from './config/conexion.js';
 
+
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 
 app.get("/", async(req, res) => {
     res.send('Api_Rest_Ful_MySql');
@@ -22,36 +24,78 @@ app.get("/users", async( req, res) => {
     }
 });
 
-app.get("/users/:id", async (req, res) => {
+// app.get("/users/:id", async (req, res) => {
+//     const sql = 'SELECT * FROM users WHERE id_user = ?';
+//     const id = req.params.id;
+//     try {
+//         const connection = await pool.getConnection();
+//         const [rows] = await connection.query(sql, [id]);
+//         connection.release();
+//         res.json(rows);
+//     } catch (error) {
+//         res.status(500).json({ error: 'Error al obtener el usuario' });
+//     }
+// });
+
+app.get("/users/:id", async(req, res) => {
+    const id = parseInt(req.params.id);
     const sql = 'SELECT * FROM users WHERE id_user = ?';
-    const id = req.params.id;
     try {
         const connection = await pool.getConnection();
         const [rows] = await connection.query(sql, [id]);
         connection.release();
-        res.json(rows);
+        (rows[0]) ? res.json(rows[0]) : res.status(404).json({ error: 'Usuario no encontrado' }); 
     } catch (error) {
         res.status(500).json({ error: 'Error al obtener el usuario' });
     }
 });
 
-app.get("/users/:id/:name", async(req, res) => {
-    res.json({ message: `Detalle del usuario con id ${req.params.id} y nombre ${req.params.name}` });
-});
-
-app.post("/users/:id", async(req, res) => {
-    res.json({ message: "Datos recibidos" });
+app.post("/users", async(req, res) => {
+    const values = req.body;
+    const sql = 'INSERT INTO users SET ?';
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query(sql, [values]);
+        connection.release();
+        console.log(rows)
+        res.send('nuevo usuario creado');
+    } catch (error) {
+        res.status(500).json({ error: 'Error al crear el usuario' });
+    }
 });
 
 app.put("/users/:id", async(req, res) => {
-    res.json({ message: "Datos actualizados" });
+    const id = req.params.id;
+    const newvalues = req.body;
+    const sql = 'UPDATE users SET ? WHERE id_user = ?';
+
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query(sql, [newvalues, id]);
+        connection.release();
+        rows.affectedRows === 0
+            ? res.status(404).json({ error: 'Usuario no encontrado' })
+            : res.json({ message: 'Usuario actualizado correctamente' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al actualizar el usuario' });
+    }
 });
 
 app.delete("/users/:id", async(req, res) => {
-    res.json({ message: `Dato con id ${req.params.id} eliminado` });
+    const id = req.params.id;
+    const sql = 'DELETE FROM users WHERE id_user = ?';
+
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query(sql, [id]);
+        connection.release();
+        rows.affectedRows === 0
+            ? res.status(404).json({ error: 'Usuario no encontrado' })
+            : res.json({ message: 'Usuario eliminado correctamente' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al eliminar el usuario' });
+    }
 });
-
-
 
 
 
